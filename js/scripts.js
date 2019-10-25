80d26e17283c2cf4f2fc3278a73c8c2f7192a257
Lela Smith <smith.lelaa@gmail.com>
2019-10-25 10:52:21 -0700

    Add js logic for quiz.

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    event.preventDefault();
+    var name = $("input#name").val();
+
+    var question1 = parseInt($("input:radio[name=question1]:checked").val());
+    var question2 = parseInt($("input:radio[name=question2]:checked").val());
+    var question3 = parseInt($("input:radio[name=question3]:checked").val());
+    var question4 = parseInt($("input:radio[name=question4]:checked").val());
+    var question5 = parseInt($("input:radio[name=question5]:checked").val());
+    var question6 = parseInt($("input:radio[name=question6]:checked").val());
+    var totalScore = question1 + question2 + question3 + question4 + question5 + question6 ;
+
+
+    if ((totalScore) <= 6) {
+      $("#result1").show();
+    }
+    else if ((totalScore) <= 12) {
+      $("#result2").show();
+    }
+    else if ((totalScore) <= 18) {
+      $("#result3").show();
+    }
+    else {
+      $("#result4").show();
+    }
+
+  });
+});
